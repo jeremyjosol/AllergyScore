25ac59421bdcf8324cd250dd296c19c7366bda9d
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-24 16:49:50 -0700

    Create Dictionary with all key value pairs

--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace Allergies.Models
 {
   public class AllergenScore
   {
+    Dictionary<string, int> AllergenScores = new Dictionary<string, int>(){
+      {"Eggs", 1},
+      {"Peanuts", 2},
+      {"Shellfish", 4},
+      {"Strawberries", 8},
+      {"Tomatoes", 16},
+      {"Chocolate", 32},
+      {"Pollen", 64},
+      {"Cats", 128}
+    };
   }
 }
 
