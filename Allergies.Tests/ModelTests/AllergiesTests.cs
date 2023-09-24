c79c2026848bd6c1cd3e382a2b6be6884fbfb9a7
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-24 16:43:00 -0700

    Add boilerplate code + prompt objective comment

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Allergies.Models;
 
-
 namespace Allergies.Tests
 {
   [TestClass]
--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -6,3 +6,14 @@ namespace Allergies.Models
   {
   }
 }
+
+
+//allergen     score
+//eggs         1
+//peanuts      2
+//shellfish    4
+//strawberries 8
+//tomatoes     16
+//chocolate    32
+//pollen       64
+//cats         128
