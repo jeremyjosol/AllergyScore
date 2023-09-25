932d98039c464d01fd415ccc9e8eac7ce4967b91
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-24 17:02:41 -0700

    Create new test for AllergenScore class, return value of 'Eggs' = 1 + pass test

--- Allergies.Tests/ModelTests/AllergiesTests.cs
+++ Allergies.Tests/ModelTests/AllergiesTests.cs
@@ -6,5 +6,11 @@ namespace Allergies.Tests
   [TestClass]
   public class AllergiesTests
   {
+    [TestMethod]
+    public void AllergenScore_ReturnsValueOfReferencedAllergen_Int()
+    {
+    int allergyScore = AllergenScore.allergenScores["Eggs"];
+    Assert.AreEqual(1, allergyScore);
+    }
   }
 }
