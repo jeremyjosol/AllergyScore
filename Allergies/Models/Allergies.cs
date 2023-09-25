cd0494ef23ced4866942b5ebe6e0324063deecee
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-24 17:40:38 -0700

    Remove comments

--- Allergies/Models/Allergies.cs
+++ Allergies/Models/Allergies.cs
@@ -40,14 +40,3 @@ namespace Allergies.Models
     }
   }
 }
-
-
-//allergen     score
-//eggs         1
-//peanuts      2
-//shellfish    4
-//strawberries 8
-//tomatoes     16
-//chocolate    32
-//pollen       64
-//cats         128
